909d20556e58d2b5cf4ae06b0a55928d1d43081f
jrhrmsll <jrhermosilla.dev@gmail.com>
2022-02-23 22:21:42 +0100

    add alqasr pac and config

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Squid    Squid    `yaml:"squid"`
+	Boundary Boundary `yaml:"boundary"`
+}
+
+type Boundary struct {
+	Controller Controller `yaml:"controller"`
+}
+
+type Controller struct {
+	Addr string `yaml:"addr"`
+	Port int    `yaml:"port"`
+}
+
+type Squid struct {
+	Port           int      `yaml:"port"`
+	AllowedDomains []string `yaml:"allowed_domains,omitempty"`
+}
+
+func Load(filename string) (*Config, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{}
+
+	err = yaml.UnmarshalStrict(content, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
